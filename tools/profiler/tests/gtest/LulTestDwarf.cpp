554451f4761829cf930e0d443147c1c1c65aeeda
Rong Bao <webmaster@csmantle.top>
2026-03-12 21:42:57 +0800

    Bug 2023167 - Part 3: Port Gecko Profiler to loongarch64-linux. r=#profiler-reviewers!
    Differential Revision: https://phabricator.services.mozilla.com/D287579

--- tools/profiler/tests/gtest/LulTestDwarf.cpp
+++ tools/profiler/tests/gtest/LulTestDwarf.cpp
@@ -2525,6 +2525,9 @@ TEST_F(LulDwarfExpr, ExpressionOverrun) {
 #elif defined(GP_ARCH_amd64) || defined(GP_ARCH_x86)
 #  define TESTED_REG_STRUCT_NAME xbp
 #  define TESTED_REG_DWARF_NAME DW_REG_INTEL_XBP
+#elif defined(GP_ARCH_loongarch64)
+#  define TESTED_REG_STRUCT_NAME fp
+#  define TESTED_REG_DWARF_NAME DW_REG_LOONGARCH_FP
 #else
 #  error "Unknown plat"
 #endif
