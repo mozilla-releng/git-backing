84d1c648173b192aa32079c50af1e4f1a09c335b
Rong Bao <webmaster@csmantle.top>
2026-03-31 16:09:50 +0800

    Bug 2023167 - Part 6: Implement RV64 and LA64 frame chasing fallback. r=#profiler-reviewers!
    This considers both the frame layout defined by JS ABI and the
    platform-native ABI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D294883

--- tools/profiler/tests/gtest/LulTest.cpp
+++ tools/profiler/tests/gtest/LulTest.cpp
@@ -2,10 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/UniquePtr.h"
+
 #include "gtest/gtest.h"
 #include "LulMain.h"
 #include "GeckoProfiler.h"       // for TracingKind
 #include "platform-linux-lul.h"  // for read_procmaps
+
+#include <cstring>
 
 // Set this to 0 to make LUL be completely silent during tests.
 // Set it to 1 to get logging output from LUL, presumably for
@@ -47,3 +51,104 @@ TEST(LulIntegration, unwind_consistency)
 
   delete lul;
 }
+
+#if defined(GP_ARCH_loongarch64) || defined(GP_ARCH_riscv64)
+TEST(LulIntegration, frame_chasing_spidermonkey_layout)
+{
+  // This tests for the transition at the boundary of JS ABI frames and native
+  // ABI frames. Currently, this is only relevant on loongarch64 and riscv64,
+  // where the two do not coincide.
+
+  lul::LUL lul(gtest_logging_sink_for_LulIntegration);
+  lul.EnableUnwinding();
+
+  // The synthetic stack for testing:
+  //
+  //                         (+)
+  //                0                   7
+  // olderNativeFP  +-------------------+ +0x180
+  //                |        ...        |
+  //      nativeFP  +-------------------+ +0x150
+  //                |   olderNativePC   |
+  //                +-------------------+
+  //                |   olderNativeFP   |
+  //                +-------------------+
+  //                |        ...        |
+  //                +-------------------+
+  //                |     nativePC      |
+  //                +-------------------+
+  //                |     nativeFP      |
+  //    enterJitFP  +-------------------+ +0x110
+  //                |  EnterJIT saves   |
+  //                |  entry args, etc. |
+  //                +-------------------+
+  //                |    enterJitPC     |
+  //                +-------------------+
+  //                |    enterJitFP     |
+  //    olderJitFP  +-------------------+ +0x50
+  //                |        ...        |
+  //                +-------------------+
+  //                |    olderJitPC     |
+  //                +-------------------+
+  //                |    olderJitFP     |
+  // youngestJitFP  +-------------------+ +0x20
+  //                |        ...        |
+  //     stackBase  +-------------------+ 0x100000
+  //                         (-)
+
+  constexpr uintptr_t stackBase = 0x100000;
+  constexpr uintptr_t youngestJitFP = stackBase + 0x20;
+  constexpr uintptr_t olderJitFP = stackBase + 0x50;
+  constexpr uintptr_t enterJitFP = stackBase + 0x110;
+  constexpr uintptr_t nativeFP = stackBase + 0x150;
+  constexpr uintptr_t olderNativeFP = stackBase + 0x180;
+
+  constexpr uintptr_t youngestJitPC = 0x1110;
+  constexpr uintptr_t olderJitPC = 0x2220;
+  constexpr uintptr_t enterJitPC = 0x3330;
+  constexpr uintptr_t nativePC = 0x4440;
+  constexpr uintptr_t olderNativePC = 0x5550;
+
+  auto stackImg = mozilla::MakeUnique<lul::StackImage>();
+  stackImg->mStartAvma = stackBase;
+  stackImg->mLen = 0x200;
+
+  const auto writeWord = [&](uintptr_t address, uintptr_t value) {
+    std::memcpy(&stackImg->mContents[address - stackBase], &value,
+                sizeof(value));
+  };
+  writeWord(youngestJitFP, olderJitFP);
+  writeWord(youngestJitFP + sizeof(uintptr_t), olderJitPC);
+  writeWord(olderJitFP, enterJitFP);
+  writeWord(olderJitFP + sizeof(uintptr_t), enterJitPC);
+  writeWord(enterJitFP, nativeFP);
+  writeWord(enterJitFP + sizeof(uintptr_t), nativePC);
+  writeWord(nativeFP - 2 * sizeof(uintptr_t), olderNativeFP);
+  writeWord(nativeFP - sizeof(uintptr_t), olderNativePC);
+
+  lul::UnwindRegs startRegs{};
+  startRegs.pc = lul::TaggedUWord(youngestJitPC);
+  startRegs.sp = lul::TaggedUWord(stackBase);
+  startRegs.fp = lul::TaggedUWord(youngestJitFP);
+
+  uintptr_t framePCs[8] = {};
+  uintptr_t frameSPs[8] = {};
+  size_t framesUsed = 0;
+  size_t framePointerFramesAcquired = 0;
+  lul.Unwind(framePCs, frameSPs, &framesUsed, &framePointerFramesAcquired, 8,
+             &startRegs, stackImg.get());
+
+  ASSERT_EQ(framesUsed, 5U);
+  EXPECT_EQ(framePointerFramesAcquired, 4U);
+  EXPECT_EQ(framePCs[0], youngestJitPC);
+  EXPECT_EQ(frameSPs[0], stackBase);
+  EXPECT_EQ(framePCs[1], olderJitPC);
+  EXPECT_EQ(frameSPs[1], youngestJitFP + 2 * sizeof(uintptr_t));
+  EXPECT_EQ(framePCs[2], enterJitPC);
+  EXPECT_EQ(frameSPs[2], olderJitFP + 2 * sizeof(uintptr_t));
+  EXPECT_EQ(framePCs[3], nativePC);
+  EXPECT_EQ(frameSPs[3], enterJitFP + 2 * sizeof(uintptr_t));
+  EXPECT_EQ(framePCs[4], olderNativePC);
+  EXPECT_EQ(frameSPs[4], nativeFP);
+}
+#endif
