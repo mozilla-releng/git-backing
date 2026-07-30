ahochheiden/firefox

Bug 2038810 - Part 10 - Assemble per-locale `chrome.manifest` files r?#build
Collects chrome.manifest entries during jar.mn staging and writes
them after all contexts finish. Chrome mode merges existing lines
so multi-locale runs accumulate per-locale entries on top of the
en-US baseline. Langpack mode also writes the per-locale
`res/multilocale.txt` consumed by toolkit's omni.ja.

Differential Revision: https://phabricator.services.mozilla.com/D299872

python/mozbuild/mozbuild/action/l10n_stage.py
@@ -21,6 +21,7 @@ from pathlib import Path
 from typing import Callable, Iterator, Optional
 
 import mozpack.path as mozpath
+from mozpack.chrome.manifest import parse_manifest_line
 
 from mozbuild.frontend.l10n_manifest import (
     MOZ_L10N_AB_CD_PLACEHOLDER,
@@ -62,6 +63,10 @@ def stage_locale(
     dest.mkdir(parents=True, exist_ok=True)
     for context in manifest.contexts:
         _stage_context(state, context)
+    _write_chrome_manifests(state)
+    if mode == "langpack":
+        _write_multilocale_txt(state)
+    _install_l10n_coverage(state)
 
 
 class StageState:
@@ -83,6 +88,38 @@ class StageState:
         self.topsrcdir = topsrcdir
         self.topobjdir = topobjdir
         self.mode = mode
+        self.manifest_entries: dict[str, list[str]] = {}
+
+
+def _write_multilocale_txt(state: StageState) -> None:
+    """Write the per-locale res/multilocale.txt consumed by toolkit's
+    omni.ja. The file is a comma-separated list of locales, always
+    including en-US. We write it explicitly because
+    mozpack.packager.l10n's non_chrome rule strips **/multilocale.txt
+    during repackaging.
+    """
+    locales = [state.locale]
+    if "en-US" not in locales:
+        locales.append("en-US")
+    out_path = state.dest / "res" / "multilocale.txt"
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    with out_path.open("w", encoding="utf-8", newline="\n") as f:
+        f.write(",".join(locales) + "\n")
+
+
+def _install_l10n_coverage(state: StageState) -> None:
+    """Copy the per-locale l10n coverage index from the merge dir into the
+    localization root so it ships in the locale's omnijar, reachable at
+    resource://gre/localization/<locale>/coverage.json. The index is
+    generated by the merge step (moz.l10n.bin.build --coverage). This is a
+    no-op for en-US, which has no merge dir.
+    """
+    coverage_src = state.merge_tree / "coverage.json"
+    if not coverage_src.is_file():
+        return
+    coverage_dst = state.dest / "localization" / state.locale / "coverage.json"
+    coverage_dst.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(coverage_src, coverage_dst)
 
 
 def _stage_context(state: StageState, context: L10nManifestContextData) -> None:
@@ -273,6 +310,26 @@ def _copy_to(src: str, dest: str) -> None:
     shutil.copyfile(src, dest)
 
 
+def _write_chrome_manifests(state: StageState) -> None:
+    """Write all collected chrome.manifest files. Each file's entries are
+    deduplicated while preserving insertion order. If a manifest file
+    already exists on disk, its lines merge ahead of the new entries so
+    chrome-mode multi-locale runs accumulate per-locale entries on top
+    of the en-US baseline (and on top of any earlier locales' entries)
+    instead of clobbering them.
+    """
+    for relpath, entries in state.manifest_entries.items():
+        path = state.dest / relpath
+        existing = []
+        if path.exists():
+            with path.open(encoding="utf-8") as f:
+                existing = [line.rstrip("\r\n") for line in f if line.strip()]
+        ordered = list(dict.fromkeys(existing + entries))
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with path.open("w", encoding="utf-8", newline="\n") as f:
+            f.write("\n".join(ordered) + "\n")
+
+
 def _stage_jar_section(
     state: StageState,
     context: L10nManifestContextData,
@@ -326,6 +383,37 @@ def _stage_jar_section(
         dest_rel = mozpath.join(install_target, section_name, entry_output)
         dest_path = mozpath.join(state.dest, dest_rel)
         _stage_entry(src, dest_path, entry.preprocess, defines)
+
+    # Chrome.manifest registration entries for the section. Entries
+    # land in <install_target>/<section.name>.manifest, with a
+    # manifest <section.name>.manifest reference added to the install
+    # target's top-level chrome.manifest.
+    if section.chrome_manifests:
+        manifest_relpath = mozpath.join(install_target, f"{section_name}.manifest")
+        chromebase = mozpath.basename(section_name) + "/"
+        base = mozpath.dirname(section_name)
+        for raw in section.chrome_manifests:
+            raw = _sub_jar(raw, subs)
+            entry = parse_manifest_line(base, raw.replace("%", chromebase))
+            state.manifest_entries.setdefault(manifest_relpath, []).append(str(entry))
+        top_manifest = mozpath.join(install_target, "chrome.manifest")
+        if top_manifest != manifest_relpath:
+            ref = f"manifest {mozpath.relpath(manifest_relpath, install_target)}"
+            state.manifest_entries.setdefault(top_manifest, []).append(ref)
+        # For sub-app contexts (staged under a subdir), also link the
+        # sub-app's chrome.manifest from the langpack root with an
+        # `application=` qualifier so `langpack_manifest.py` can recurse
+        # into the sub-app dir when populating `manifest.json`'s
+        # `chrome_resources`. Skipped in chrome mode: that path stages
+        # into `dist/bin`, where the sub-app's chrome.manifest is
+        # discovered by the runtime chrome registry without a root
+        # reference.
+        if context.install_subdir and state.mode == "langpack":
+            sub_chrome = mozpath.join(context.install_subdir, "chrome.manifest")
+            appid = state.manifest.moz_app_id
+            state.manifest_entries.setdefault("chrome.manifest", []).append(
+                f"manifest {sub_chrome} application={appid}"
+            )
 
 
 def _resolve_jar_install_target(

mozilla-releng/git-backing

python/mozbuild/mozbuild/action/l10n_stage.py
@@ -21,6 +21,7 @@ from pathlib import Path
 from typing import Callable, Iterator, Optional
 
 import mozpack.path as mozpath
+from mozpack.chrome.manifest import parse_manifest_line
 
 from mozbuild.frontend.l10n_manifest import (
     MOZ_L10N_AB_CD_PLACEHOLDER,
@@ -62,6 +63,10 @@ def stage_locale(
     dest.mkdir(parents=True, exist_ok=True)
     for context in manifest.contexts:
         _stage_context(state, context)
+    _write_chrome_manifests(state)
+    if mode == "langpack":
+        _write_multilocale_txt(state)
+    _install_l10n_coverage(state)
 
 
 class StageState:
@@ -83,6 +88,38 @@ class StageState:
         self.topsrcdir = topsrcdir
         self.topobjdir = topobjdir
         self.mode = mode
+        self.manifest_entries: dict[str, list[str]] = {}
+
+
+def _write_multilocale_txt(state: StageState) -> None:
+    """Write the per-locale res/multilocale.txt consumed by toolkit's
+    omni.ja. The file is a comma-separated list of locales, always
+    including en-US. We write it explicitly because
+    mozpack.packager.l10n's non_chrome rule strips **/multilocale.txt
+    during repackaging.
+    """
+    locales = [state.locale]
+    if "en-US" not in locales:
+        locales.append("en-US")
+    out_path = state.dest / "res" / "multilocale.txt"
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    with out_path.open("w", encoding="utf-8", newline="\n") as f:
+        f.write(",".join(locales) + "\n")
+
+
+def _install_l10n_coverage(state: StageState) -> None:
+    """Copy the per-locale l10n coverage index from the merge dir into the
+    localization root so it ships in the locale's omnijar, reachable at
+    resource://gre/localization/<locale>/coverage.json. The index is
+    generated by the merge step (moz.l10n.bin.build --coverage). This is a
+    no-op for en-US, which has no merge dir.
+    """
+    coverage_src = state.merge_tree / "coverage.json"
+    if not coverage_src.is_file():
+        return
+    coverage_dst = state.dest / "localization" / state.locale / "coverage.json"
+    coverage_dst.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(coverage_src, coverage_dst)
 
 
 def _stage_context(state: StageState, context: L10nManifestContextData) -> None:
@@ -273,6 +310,26 @@ def _copy_to(src: str, dest: str) -> None:
     shutil.copyfile(src, dest)
 
 
+def _write_chrome_manifests(state: StageState) -> None:
+    """Write all collected chrome.manifest files. Each file's entries are
+    deduplicated while preserving insertion order. If a manifest file
+    already exists on disk, its lines merge ahead of the new entries so
+    chrome-mode multi-locale runs accumulate per-locale entries on top
+    of the en-US baseline (and on top of any earlier locales' entries)
+    instead of clobbering them.
+    """
+    for relpath, entries in state.manifest_entries.items():
+        path = state.dest / relpath
+        existing = []
+        if path.exists():
+            with path.open(encoding="utf-8") as f:
+                existing = [line.rstrip("\r\n") for line in f if line.strip()]
+        ordered = list(dict.fromkeys(existing + entries))
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with path.open("w", encoding="utf-8", newline="\n") as f:
+            f.write("\n".join(ordered) + "\n")
+
+
 def _stage_jar_section(
     state: StageState,
     context: L10nManifestContextData,
@@ -326,6 +383,37 @@ def _stage_jar_section(
         dest_rel = mozpath.join(install_target, section_name, entry_output)
         dest_path = mozpath.join(state.dest, dest_rel)
         _stage_entry(src, dest_path, entry.preprocess, defines)
+
+    # Chrome.manifest registration entries for the section. Entries
+    # land in <install_target>/<section.name>.manifest, with a
+    # manifest <section.name>.manifest reference added to the install
+    # target's top-level chrome.manifest.
+    if section.chrome_manifests:
+        manifest_relpath = mozpath.join(install_target, f"{section_name}.manifest")
+        chromebase = mozpath.basename(section_name) + "/"
+        base = mozpath.dirname(section_name)
+        for raw in section.chrome_manifests:
+            line = _sub_jar(raw, subs)
+            entry = parse_manifest_line(base, line.replace("%", chromebase))
+            state.manifest_entries.setdefault(manifest_relpath, []).append(str(entry))
+        top_manifest = mozpath.join(install_target, "chrome.manifest")
+        if top_manifest != manifest_relpath:
+            ref = f"manifest {mozpath.relpath(manifest_relpath, install_target)}"
+            state.manifest_entries.setdefault(top_manifest, []).append(ref)
+        # For sub-app contexts (staged under a subdir), also link the
+        # sub-app's chrome.manifest from the langpack root with an
+        # `application=` qualifier so `langpack_manifest.py` can recurse
+        # into the sub-app dir when populating `manifest.json`'s
+        # `chrome_resources`. Skipped in chrome mode: that path stages
+        # into `dist/bin`, where the sub-app's chrome.manifest is
+        # discovered by the runtime chrome registry without a root
+        # reference.
+        if context.install_subdir and state.mode == "langpack":
+            sub_chrome = mozpath.join(context.install_subdir, "chrome.manifest")
+            appid = state.manifest.moz_app_id
+            state.manifest_entries.setdefault("chrome.manifest", []).append(
+                f"manifest {sub_chrome} application={appid}"
+            )
 
 
 def _resolve_jar_install_target(